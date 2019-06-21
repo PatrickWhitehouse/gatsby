9482d7cf62e774d71f57c87fadb5e5f341b884bf
Patrick Whitehouse <patrick1996@gmail.com>
2019-06-21 13:17:29 +0100

    add 404

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Layout from "../components/layout"
+import { Helmet } from "react-helmet"
+
+export default () => (
+    <Layout>
+        <h1>Oops, page not found.</h1>
+    </Layout>
+)
